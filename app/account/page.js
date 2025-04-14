fb7925e8aa166a2e221169c877017fbc1d4276bd
AmitPant9999 <heyamitpant9999@gmail.com>
2025-04-14 20:35:25 +0530

    Update page.js

--- app/account/page.js
+++ app/account/page.js
@@ -10,7 +10,7 @@ export default async function Page() {
   const firstName = session.user.name.split(" ").at(0);
 
   return (
-    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+    <h2 className="font-semibold text-2xl sm:text-3xl lg:text-4xl text-accent-400 mb-4 sm:mb-6 lg:mb-7">
       Welcome, {firstName}
     </h2>
   );
