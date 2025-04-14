db4e5477408e8af8b8c360afa61a9f9143a264ed
AmitPant9999 <heyamitpant9999@gmail.com>
2025-04-14 20:33:11 +0530

    Update page.js

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -11,13 +11,13 @@ export default async function Page() {
   const bookings = await getBookings(session.user.guestId);
 
   return (
-    <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+    <div className="px-6 sm:px-8 lg:px-16">
+      <h2 className="font-semibold text-2xl sm:text-3xl text-accent-400 mb-6 sm:mb-8">
         Your reservations
       </h2>
 
       {bookings.length === 0 ? (
-        <p className="text-lg">
+        <p className="text-lg sm:text-xl">
           You have no reservations yet. Check out our{" "}
           <a className="underline text-accent-500" href="/cabins">
             luxury cabins &rarr;
